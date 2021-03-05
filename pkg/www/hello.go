7cc69f5d336bcc910b4faae1a583ca53516e6f8a
balamuru <balamuru@gmail.com>
2021-03-05 00:10:36 -0600

    first commit

---
+++ pkg/www/hello.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	}).Methods("GET")
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hi " + time.Now().String())
+	}).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
+}
+
