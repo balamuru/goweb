9e734bb79dac11dded098b74d3f208ff833fbe25
balamuru <balamuru@gmail.com>
2021-03-06 22:23:28 -0600

    use modules

--- pkg/app/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/balamuru/goweb/pkg/app
-
-go 1.16
-
-require github.com/gorilla/mux v1.8.0 // indirect
--- pkg/app/server.go
+++ pkg/app/server.go
@@ -4,7 +4,6 @@ import (
 	"github.com/balamuru/goweb/pkg/utils"
 	"fmt"
 	"net/http"
-	"time"
 	"github.com/gorilla/mux"
 )
 
@@ -12,14 +11,15 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hi " + time.Now().String())
-		NowTime()
+		fmt.Fprintln(w, "Hi, the time is now: " + utils.NowTime())
+		
+		
 	}).Methods("GET")
 
 	r.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		fmt.Println(vars["name"])
-		fmt.Fprintln(w, "Hi " + time.Now().String())
+		fmt.Fprintln(w, "Hi ", vars["name"] , ". the time is now: " + utils.NowTime() )
 	}).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
